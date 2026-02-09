ab6aff320c7b5bfad6a648113044400775a77aa0
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2026-02-09 16:24:01 -0400

    ajustes nas rotas.

--- routes/web.php
+++ routes/web.php
@@ -5,25 +5,20 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\VersioningController;
 use App\Http\Middleware\isAdmin;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    if (Auth::check()) {
-        return redirect()->route('versionings.index');
-    } else {
-        return redirect()->route('login');
-    }
+    return Auth::check() ? redirect()->route('versionings.index') : redirect()->route('login');
 });
 
-Route::resource('projects', ProjectController::class);
-Route::resource('versionings', VersioningController::class);
-Route::resource('users',UserController::class);
-// Route::resource('users', UserController::class)
-//     ->middleware(isAdmin::class)
-//     ->except(['show']);
-// Route::get('users/{user}', [UserController::class, 'show'])
-//     ->name('users.show');
+Route::resource('projects', ProjectController::class)->middleware(['auth']);
+Route::resource('versionings', VersioningController::class)->middleware(['auth']);
+
+Route::resource('users', UserController::class)
+    ->middleware(isAdmin::class)
+    ->except(['show']);
+Route::get('users/{user}', [UserController::class, 'show'])
+    ->middleware('auth')
+    ->name('users.show');
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
